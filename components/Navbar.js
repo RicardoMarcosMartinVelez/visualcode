576325af95973dd490e7e7e4cbf138956c240c46
ElMarto <velez.marcos.mv@gmail.com>
2023-08-16 01:28:24 -0300

    ..qw

---
+++ components/Navbar.js
@@ -0,0 +1,23 @@
+// components/Navbar.js
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
+
